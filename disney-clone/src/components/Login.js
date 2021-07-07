6e526915e55e3d34458144a0fc68d957416fc8d9
anandpratiksha <apratiksha1137@gmail.com>
2021-07-07 20:36:00 +0530

    Clone

---
+++ disney-clone/src/components/Login.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components'
+
+const Login = (props) => {
+    return (
+        <Container>
+            <Content>
+                <CTA>
+                    <CTALogoOne src="/images/cta-logo-one.png" alt="" />
+                </CTA>
+                <BgImage />
+            </Content>
+        </Container>
+    )
+};
+
+const Container = styled.section`
+overflow:hidden;
+display:flex;
+flex-direction:column;
+text-align:center;
+height:100vh;
+`;
+
+const Content = styled.div`
+margin-bottom:10vw;
+width:100%;
+position:relative;
+min-height:100vh;
+box-sizing:border-box;
+display:flex;
+justify-content:center;
+align-items:center;
+padding:80px 40px;
+`;
+
+const BgImage = styled.div`
+background-image:url('/images/loginbackground.jpg');
+height:100%;
+background-position:top;
+background-size:cover;
+background-repeat:no-repeat;
+position:absolute;
+top:0;
+right:0;
+left:0;
+z-index:-1;
+`;
+
+const CTA = styled.div`
+margin-bottom:2vw;
+max-width:650px;
+flex-wrap: wrap;
+display:flex;
+flex-direction:column;
+justify-content: center;
+margin-top:0;
+align-items: center;
+text-align: center;
+margin-right:auto;
+margin-left:auto;
+transtion-timing-function:ease-out;
+transition:opacity 0.2s;
+width:100%;
+`;
+
+const CTALogoOne = styled.img`
+margin-bottom:12px;
+${'' /* max-width:60px; */}
+${'' /* min-height:1px; */}
+display:block;
+width:100%;
+`;
+
+export default Login;
