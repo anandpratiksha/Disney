cbaa107546d732168cf150aa50045e07420bc451
anandpratiksha <apratiksha1137@gmail.com>
2021-07-09 14:35:24 +0530

    Clone

--- disney-clone/src/components/Home.js
+++ disney-clone/src/components/Home.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import ImgSlider from './ImgSlider'
+import ImgSlider from '../components/ImgSlider'
 
 const Home = (props) => {
     return (
--- disney-clone/src/components/ImgSlider.js
+++ disney-clone/src/components/ImgSlider.js
@@ -1,20 +1,63 @@
 import styled from 'styled-components'
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css'
-import slider from 'raect-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
 
 const ImgSlider = (props) => {
-    // let settings = {
-    //     dots: true,
-    //     infinite: true,
-    //     speed: 500,
-    //     slideToShow: 1,
-    //     slidesToScroll: 1,
-    //     autoplay: true,
-    // }
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slideToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+    }
     return (
-        <div>Image slider</div>
-    )
+        <Carousel {...settings}>
+            <div>
+                <h3>1</h3>
+            </div>
+            <div>
+                <h2>2</h2>
+            </div>
+            <div>
+                <h2>3</h2>
+            </div>
+        </Carousel>
+    );
 };
+
+
+const Carousel = styled(Slider)`
+margin-top:20px;
+
+& > button {
+    opacity:0;
+    height:100%;
+    width:5vw;
+    z-index:1;
+
+&:hover{
+    opacity:1;
+    transition:opacity 0.2s ease 0s;
+}
+}
+
+ul li button { 
+    &:before{
+        font-size:10px;
+        color:rgba(150,158,171);
+    }
+}
+
+li.slick-active button:before {
+    color:white;
+}
+ 
+.slick-list {
+    overflow:initial;
+}
+
+`;
 
 export default ImgSlider;
